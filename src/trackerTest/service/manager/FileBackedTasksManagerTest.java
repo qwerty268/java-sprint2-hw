a971b3b34355e2f059062157b90a5359acc2d95c
qwerty_268 <lev-demchenko@bk.ru>
2022-03-18 19:19:05 +0300

    feat: все тесты добавлены

--- src/trackerTest/service/manager/FileBackedTasksManagerTest.java
+++ src/trackerTest/service/manager/FileBackedTasksManagerTest.java
@@ -1,36 +1,4 @@
-package trackerTest.service.manager;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import tracker.service.manager.inMemoryManager.InMemoryTasksManager;
-
-class FileBackedTasksManagerTest implements BaseTaskManagerTest {
-
-    private InMemoryTasksManager tasksManager;
-
-    @BeforeEach
-    public void createTaskManager() {
-        tasksManager = new InMemoryTasksManager();
-    }
-
-    @Test
-    @Override
-    public void testAddingAnyTypeOfTask() {
-
-    }
-
-    @Override
-    public void testUpdatingTask() {
-
-    }
-
-    @Override
-    public void testDeletingAll() {
-
-    }
-
-    @Override
-    public void testDeletingByIndex() {
-
-    }
+import static org.junit.jupiter.api.Assertions.*;
+class FileBackedTasksManagerTest {
+  
 }
--- src/trackerTest/service/manager/InMemoryTasksManagerTest.java
+++ src/trackerTest/service/manager/InMemoryTasksManagerTest.java
@@ -1,59 +1,7 @@
-package trackerTest.service.manager;
+package trackerTest.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import tracker.model.Status;
-import tracker.model.Task;
-import tracker.service.manager.inMemoryManager.InMemoryTasksManager;
+import static org.junit.jupiter.api.Assertions.*;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.List;
+class InMemoryTasksManagerTest {
 
-class InMemoryTasksManagerTest implements BaseTaskManagerTest {
-
-    private InMemoryTasksManager tasksManager;
-
-    @BeforeEach
-    public void createTaskManager() {
-        tasksManager = new InMemoryTasksManager();
-    }
-
-    @Test
-    @Override
-    public void testAddingAnyTypeOfTask() {
-        Task task1 = new Task(1, "task1", "task1", Status.DONE, Duration.ofSeconds(10),
-                LocalDateTime.of(2017, 11, 11, 1, 1));
-
-        tasksManager.addAnyTypeOfTask(task1);
-
-        Assertions.assertEquals(tasksManager.getTask(1), task1);
-    }
-
-    @Test
-    @Override
-    public void testUpdatingTask() {
-        Task task1 = new Task(1, "task1", "task1", Status.DONE, Duration.ofSeconds(10),
-                LocalDateTime.of(2017, 11, 11, 1, 1));
-
-        tasksManager.addAnyTypeOfTask(task1);
-
-        task1 = new Task(1, "task2", "task2", Status.DONE, Duration.ofSeconds(10),
-                LocalDateTime.of(2018, 11, 11, 1, 1));
-
-        tasksManager.updateTask(task1);
-
-        Assertions.assertEquals(tasksManager.getTask(1), task1);
-    }
-
-    @Override
-    public void testDeletingAll() {
-
-    }
-
-    @Override
-    public void testDeletingByIndex() {
-
-    }
 }
